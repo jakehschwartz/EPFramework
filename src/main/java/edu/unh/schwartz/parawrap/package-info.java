d0b9a24644e979bcd84a296ab9f48a3ad1dfed4b
jakehschwartz <jakehschwartz@gmail.com>
2013-11-05 19:49:51 -0500

    Removed old files

--- src/main/java/edu/unh/schwartz/parawrap/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Contains the application class and other classes used throughout the program.
- */
-package edu.unh.schwartz.parawrap;
--- src/main/java/edu/unh/schwartz/parawrap/worker/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Classes to do the execution on the work and hold statistics about the
- * runtimes and length of the work.
- */
- package edu.unh.schwartz.parawrap.worker;
